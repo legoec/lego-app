953604be6dab1a5c595de38591fad704f322c662
Darwin Gualotuña <daosgc@gmail.com>
2020-04-24 20:11:29 -0500

    Remove mockdata

--- src/app/shared/vendors/vendors.component.ts
+++ src/app/shared/vendors/vendors.component.ts
@@ -16,27 +16,8 @@ export class VendorsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    const mockVendors = [
-      {
-        economic_activity: 'Software Developer',
-        legal_representative: 'Sultano de Loja',
-        logo: 'https://cdn0.iconfinder.com/data/icons/occupation-002/64/freelance-nomad-occupation-avatar-512.png',
-        slogan: 'Apasionado por la tecnologia, 5 años de experiencia',
-        status: VendorStatus.PENDING
-      },
-      {
-        economic_activity: 'Software Backend',
-        legal_representative: 'Fulana de Tal',
-        logo: 'https://cdn0.iconfinder.com/data/icons/occupation-001/64/designer-freelance-occupation-avatar-512.png',
-        slogan: 'Apasionado por devops y con 2 años de experiencia en el lado del servidor',
-        status: VendorStatus.APPROVED
-      }
-    ];
     this.vendorsService.getVendors().subscribe( response => {
-      this.vendors = [
-        ...response,
-        ...mockVendors
-      ];
+      this.vendors = response;
     });
   }
 
