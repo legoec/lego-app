151fd8a7cb308779936ae9615736b07513d42eb7
Fabricio Flores <fabrifloresg@gmail.com>
2020-05-08 15:35:59 -0500

    Add modal to submit a feedback

---
+++ src/app/shared/modal-feedback/modal-feedback.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-modal-feedback',
+  templateUrl: './modal-feedback.component.html',
+  styleUrls: ['./modal-feedback.component.scss'],
+})
+export class ModalFeedbackComponent {
+  feedback: String
+
+  constructor(private modalInstance: ModalController){
+  }
+
+  onSubmit():void {
+    this.modalInstance.dismiss({
+        feedback: this.feedback
+    });
+  }
+}
