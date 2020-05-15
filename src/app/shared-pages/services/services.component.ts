e0c3c108d9f3963f19f33cfedb26474fc13b28f7
Darwin Gualotuña <daosgc@gmail.com>
2020-05-15 14:06:27 -0500

    Add infinitescroll in services client

--- src/app/shared-pages/services/services.component.ts
+++ src/app/shared-pages/services/services.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
 import { Service } from 'src/app/models/service';
@@ -6,7 +6,6 @@ import { map } from 'rxjs/operators';
 import { ServicesService } from 'src/app/shared/services/services.service';
 import { Category } from 'src/app/models/category';
 import { CategoriesService } from 'src/app/shared/services/categories.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-services',
@@ -15,8 +14,10 @@ import { Observable } from 'rxjs';
 })
 export class ServicesPageComponent implements OnInit {
 
-  services$: Observable<Service[]>;
+  services: Service[];
   category: Category;
+  query: string;
+  currentPage: number;
 
   constructor(
     private servicesService: ServicesService,
@@ -26,16 +27,35 @@ export class ServicesPageComponent implements OnInit {
 
   ngOnInit() {
     this.activatedRoute.params.pipe(
-      map(param => parseInt(param.id))).subscribe(id => {
-      this.services$ = this.servicesService.getServicesFromCategory(id);
+      map(param => parseInt(param.id, 10))).subscribe(id => {
+      this.servicesService.getServicesFromCategory(id).subscribe(servicesData => {
+        this.services = servicesData.services;
+      });
       this.categoryService.getCategory(id).subscribe(category => {
         this.category = category;
       });
     });
+    this.currentPage = 1;
   }
 
   onSearch(query: string) {
-    this.services$ = this.servicesService.getServicesFromCategory(this.category.id, query);
+    this.query = query;
+    this.currentPage = 1;
+    this.servicesService.getServicesFromCategory(this.category.id, query, this.currentPage).subscribe(servicesData => {
+      this.services = servicesData.services;
+    });
+  }
+
+  loadData(event) {
+    this.currentPage = this.currentPage + 1;
+    this.servicesService.getServicesFromCategory(this.category.id, this.query, this.currentPage).subscribe(servicesData => {
+      this.services = this.services.concat(servicesData.services);
+      if (this.services.length <= servicesData.total_count) {
+        event.target.complete();
+      } else {
+        event.target.disabled = true;
+      }
+    });
   }
 
 }
