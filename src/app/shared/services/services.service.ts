2c37ec5de2a6fba1e56aadff0da50fe86746d859
Darwin Gualotuña <daosgc@gmail.com>
2020-05-15 14:58:52 -0500

    Add infinite scroll in services client (#26)
    * Add infinitescroll in services client
    
    * Add minor improvements

--- src/app/shared/services/services.service.ts
+++ src/app/shared/services/services.service.ts
@@ -5,15 +5,20 @@ import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Service } from '../../models/service';
 
+interface ServicePagination {
+  services: Service[];
+  total_count: number;
+}
+
 @Injectable()
 export class ServicesService {
   constructor(private http: HttpClient) { }
 
   private baseUrl: string = `${environment.apiBase}/v0`;
 
-  getServicesFromCategory(categoryId: number, query: string = ''): Observable<Service[]> {
-    const params = new HttpParams().set('query', query);
-    return this.http.get<Service[]>(`${this.baseUrl}/categories/${categoryId}/services`, { params });
+  getServicesFromCategory(categoryId: number, query: string = '', page: number = 1): Observable<ServicePagination> {
+    const params = new HttpParams().set('query', query).set('page', `${page}`);
+    return this.http.get<ServicePagination>(`${this.baseUrl}/categories/${categoryId}/services`, { params });
   }
 
   addService(service: Service): Observable<Service> {
