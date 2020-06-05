d3ab9657c700a13e29462f8d8169d9a742bf40b2
Fabricio Flores <fabrifloresg@gmail.com>
2020-06-05 15:04:25 -0500

    now is working

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url: 'https://legoec-app.herokuapp.com/',
-  apiBase: 'https://legoec-api.herokuapp.com',
+  apiBase: 'https://legoec-api.herokuapp.com/api',
 };
